510d794c69a55cecad450aa0bb136b55dbd008d5
Egor <znogoud3@gmail.com>
2019-10-11 22:49:04 +0300

    feat: doubly-linked-list solution

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,74 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.list = [];
+    }
 
-    append(data) {}
+    get length() {
+        return this.list.length;
+    }
 
-    head() {}
+    get _head() {
+        return this.list[0];
+    }
 
-    tail() {}
+    get _tail() {
+        const last = this.list.length - 1;
+        return this.list[last];
+    }
 
-    at(index) {}
 
-    insertAt(index, data) {}
+    append(data) {
+        this.list.push(new Node(data));
+        return this;
+    }
 
-    isEmpty() {}
+    head() {
+        return this.list.length !== 0 ? this.list[0].data : null;
+    }
 
-    clear() {}
+    tail() {
+        return this.list.length !== 0 ? this.list[this.list.length - 1].data : null;
+    }
 
-    deleteAt(index) {}
+    at(index) {
+        return this.list.length !== 0
+            && this.list.length > index && index >= 0 ?
+            this.list[index].data : null;
+    }
 
-    reverse() {}
+    insertAt(index, data) {
+        this.list.splice(index, 0, new Node(data));
+    }
 
-    indexOf(data) {}
+    isEmpty() {
+        return this.list.length === 0;
+    }
+
+    clear() {
+        this.list.length = 0;
+        return this;
+    }
+
+    deleteAt(index) {
+        this.list.splice(index, 1);
+        return this;
+    }
+
+    reverse() {
+        this.list.reverse();
+        return this;
+    }
+
+    indexOf(data) {
+        for (let index = 0; index < this.list.length; index++) {
+            if (this.list[index].data === data) {
+                return index;
+            }
+        }
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
